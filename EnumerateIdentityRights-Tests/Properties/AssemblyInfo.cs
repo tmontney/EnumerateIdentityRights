c5966f66b79c321dc79b2f387783a21366c0b480
Tyler Montney <tyler.montney@w3g.mylocal>
2022-02-26 20:53:44 -0600

    initial commit

---
+++ EnumerateIdentityRights-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnumerateIdentityRights-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnumerateIdentityRights-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d1b1043-540a-417b-a67e-0c26ada47bcd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EnumerateIdentityRights-UI/EnumerateIdentityRights.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerateIdentityRights
+{
+    internal class EnumerateIdentityRights_UI
+    {
+        static void Main(string[] args)
+        {
+            int exit_code = 0;
+            string identity = Environment.UserName;
+
+            if (args.Length == 1)
+            {
+                identity = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Argument position 0 not specified; defaulting to current user '" + identity + "'");
+                Console.WriteLine();
+            }
+
+            try
+            {
+                using (EnumerateIdentityRights.LsaWrapper lsa = new EnumerateIdentityRights.LsaWrapper())
+                {
+                    Console.WriteLine("Identity '" + identity + "' contains the following rights (if any)");
+                    Console.WriteLine("*\t" + string.Join("\n*\t", lsa.GetPrivileges(identity)));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Re-run this application using an account which has administrative privileges");
+                exit_code = 5;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception was thrown: " + ex.Message);
+                exit_code = 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            Environment.Exit(exit_code);
+        }
+    }
+}
